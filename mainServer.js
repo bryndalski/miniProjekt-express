0f69d306c3beee79325b93e2fe495a164fc94d79
bryndalski <kubabryndal@gmail.com>
2020-11-03 22:46:56 +0100

    UPDATE 2
    dodano pełną unkcjonalność login/out z zabwzpieczeniami
    dodać autowylogowywanie po zamknięciu serwera
    dodano testową tablice USUNĄĆ NA KONIEC
    dodano opcje show
    przeniosłem na koniec skryptu default page errora

--- mainServer.js
+++ mainServer.js
@@ -1,24 +1,104 @@
 //zmienne, stałe
-//TODO ustaw zależnośc łądowania strony od cookie w routingu dla adminan
-//TODO działa ustawnianie funckji nie działą czytanie  == linijka 20 
 var express = require("express")
 var app = express()
-const PORT = 3000;
+const PORT = 5500;
 // routing stuff 
 var path = require("path")
 app.use(express.static('static'))
 //form stuff
 var bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({ extended: true }));
-let userDataArray = []
+// let userDataArray = []
+let userDataArray = [
+    {
+        login: 'Maciek',
+        password: '123',
+        ageSelect: '17',
+        uczen: 'on',
+        plec: 'on'
+    },
+    {
+        login: '123',
+        password: '123',
+        ageSelect: '16',
+        uczen: 'on',
+        plec: 'on'
+    },
+    {
+        login: '12',
+        password: '22',
+        ageSelect: '3333',
+        uczen: 'off',
+        plec: 'on'
+    },
+    {
+        login: 'bryndalski',
+        password: '123',
+        ageSelect: '19',
+        uczen: 'on',
+        plec: 'on'
+    },
+    {
+        login: '4124124',
+        password: '141241',
+        ageSelect: '12',
+        uczen: 'on',
+        plec: 'on'
+    },
+    {
+        login: 'Maciek',
+        password: '123',
+        ageSelect: '17',
+        uczen: 'on',
+        plec: 'on'
+    },
+    {
+        login: '123',
+        password: '123',
+        ageSelect: '16',
+        uczen: 'on',
+        plec: 'on'
+    },
+    {
+        login: '12',
+        password: '22',
+        ageSelect: '3333',
+        uczen: 'off',
+        plec: 'on'
+    },
+    {
+        login: 'bryndalski',
+        password: '123',
+        ageSelect: '19',
+        uczen: 'on',
+        plec: 'on'
+    },
+    {
+        login: '4124124',
+        password: '141241',
+        ageSelect: '12',
+        uczen: 'on',
+        plec: 'on'
+    },
+    {
+        login: 'kubabryndal@gmail.com',
+        password: '1231',
+        ageSelect: '21',
+        uczen: 'on',
+        plec: 'on'
+    }
+]
+
 //logIn Stuff
 var express = require('express')
-var cookieParser = require('cookie-parser')
-
+var cookieParser = require('cookie-parser');
+const { S_IFBLK } = require("constants");
+app.use(cookieParser())
+//Admin stuff
+const pageBasic = "<body style='background-color:black;width:100vw;height:100vh'><a href='/sort'>Sort</a><a href='gender'>gender</a><a href='show'>Show</a>"
 // routingi odpowiednich stron
 app.get("/admin", function (req, res) { // dla admina z warunkiem na cookies
-    console.log(req.cookies)
-    if (req.cookies == undefined)
+    if (req.cookies.LogData == undefined)
         res.sendFile(path.join(__dirname + "/static/pages/adminPage.html"))
     else
         res.sendFile(path.join(__dirname + "/static/pages/adminLoggedPage.html"))
@@ -35,15 +115,10 @@ app.get("/main", function (req, res) { // dla maina
 app.get("/", function (req, res) { // dla maina bez nazwy 
     res.sendFile(path.join(__dirname + "/static/pages/mainPage.html"))
 })
-// obsługa routingu który nie został podany zwraca error 404
-app.get('*', function (req, res) {
-    res.status(404).send('Przykro mi, wystąpił błąd :(((    ');
-});
-//obsługa pobierania formularzy 
 
+//obsługa pobierania formularzy 
 // dla formularza obsługi danych użytkownika 
 app.post("/RegisterFromula", function (req, res) {
-    console.log(req.body)
     if (userDataArray.findIndex(array => array.login === req.body.login) == "-1" && req.body.login != '' && req.body.password != '') {  // kontroluje brak powtórzenia loginu wymagane pole hasła i loginu nie jest puste  jeśli jakiś super user nie pochwali się jakim jest hakerem i nie usunie w konsoli input required
         userDataArray.push(req.body)
         res.sendFile(path.join(__dirname + "/static/pages/registerPage.html"))
@@ -53,20 +128,46 @@ app.post("/RegisterFromula", function (req, res) {
 })
 //logIn formula
 app.post('/LoginFormula', function (req, res) {
-    console.log(req.body)
     let searchiedindex = userDataArray.findIndex(array => array.login === req.body.login)
     if (searchiedindex != '-1') { // oznaczenie to oznacza że został znaleziowny w tablicy / bazie danych 
         res
-            .cookie("LogData", "User is logged" + req.body.login, { httpOnly: true })
-            .send()
-        console.log("ładowanie lepszego widoku")
+            .cookie("LogData", req.body.login, { httpOnly: true })
+            .redirect('/admin');
     } else {
         res.send("<h1>Dane logowania niepoprawne </h1><a href='main'>przejdź na stronę główną</a>")
     }
 })
+//obsługa adminPanela
+app.get("/logOut", function (req, res) { // dla maina bez nazwy 
+    if (req.cookies.LogData == undefined)
+        res.sendFile(path.join(__dirname + "/static/pages/errorPage.html"))
+    console.log(res.cookie)
+    res
+        .clearCookie("LogData")
+        .redirect('/main')
+})
 
-
-
+//obsługa podanych stringów na serwerze 
+app.get('/show', function (req, res) {
+    if (req.cookies.LogData != undefined) {
+        let tempPage = pageBasic + "<table style='width:80vw;height:60vh ' >"
+        for (i = 0; i < userDataArray.length; i++) {// tworzę stringa z tabelą
+            tempPage += "<tr style='background-color:rgb(36, 36, 37);color:ghostwhite;border:solid 1px ghostwhite' ><th style='background-color:rgb(36, 36, 37);color:ghostwhite;border:solid 1px ghostwhite'>" + i + "</th><th style='background-color:rgb(36, 36, 37);color:ghostwhite; border:solid 1px ghostwhite'>" + userDataArray[i].login + " - " + userDataArray[i].password + "</th>"
+            if (userDataArray[i].uczen == "on")
+                tempPage += "<th style='background-color:rgb(36, 36, 37);color:ghostwhite; border:solid 1px ghostwhite'>Uczeń : <input type='checkbox' checked disabled></th>"
+            else
+                tempPage += "<th style='background-color:rgb(36, 36, 37);color:ghostwhite; border:solid 1px ghostwhite'>Uczeń : <input type='checkbox' disabled></th>"
+            tempPage += "<th style='background-color:rgb(36, 36, 37);color:ghostwhite; border:solid 1px ghostwhite'> Wiek: " + userDataArray[i].ageSelect + "</th>" + "<th style='background-color:rgb(36, 36, 37);color:ghostwhite; border:solid 1px ghostwhite'> płeć: " + userDataArray[i].plec + "</th></tr>"
+        }
+        tempPage += "</table></body>"
+        res.send(tempPage)
+    } else
+        res.sendFile(path.join(__dirname + "/static/pages/errorPage.html"))
+})
+// obsługa routingu który nie został podany zwraca stronę, z stosowną informacją ( umieszczam na końcu coś w stulu switcha i opcja default)
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname + "/static/pages/errorPage.html"))
+});
 
 //nasłuch na określonym porcie
 app.listen(PORT, function () {
